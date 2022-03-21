b4e6ab175f3ba2e75b1673c4a389863a6de38c25
jamesthomasbester <46641259+jamesthomasbester@users.noreply.github.com>
2022-03-21 15:59:45 +1100

    final changes
    finshed the todo's.
    the project is done with this final update, i've added the highscores functions and code refine.

--- script.js
+++ script.js
@@ -43,36 +43,89 @@ const quizQuestions =[
     },
 ];
 
+var highscore = 
+[
+    {
+        name: "John Doe",
+        score: 32
+    },
+    {
+        name: "Jane Smith",
+        score: 66
+    }
+
+]
 //declearing global variables
 var index = 0;
+var highscoreToggle = false;
 var score = 0;
 var count = 60;
+var start = false;
+var stopMultipleInputs = 0;
+const QC = document.getElementById('question-card');
+const HC = document.getElementById('highscores-list');
+
 
 /*
 this is the initial function that is called when the start button is clicked,
-
  */
 function quiz(){
-    timer();
+    start = true;
     createQuestionCard();
     document.getElementById("start-card").style.display="none";
-    document.getElementById("question-card").style.display="block";
+    QC.style.display="block";
 }
 
+/*
+    an onclick listener for the view highscores button that displays a highscores card with 2 auto-generated scores,
+    as well as any new scores entered. that are cached in localstoraged.
+*/
+document.getElementById('highscores').addEventListener('click', function(){
+    let alpha;
+    if (!highscoreToggle){
+        HC.style.display = "inline";
+        highscoreToggle = true;
+    }else{
+        HC.style.display = "none";
+        highscoreToggle = false;
+    }
+    HC.innerHTML = "";
+    if(JSON.parse(localStorage.getItem('highscores')) == null){
+        alpha = highscore;
+        console.log("null");
+    }else{
+        alpha = JSON.parse(localStorage.getItem('highscores'));
+        console.log(alpha);
+    }
+    let beta = [];
+    alpha.forEach(element => {
+        beta.push(
+            `
+            <li>
+                <p>${element.name}</p>
+                <p>${element.score}</p>
+            </li>
+            `
+        )
+        HC.innerHTML += beta[beta.length -1];
+    });
+
+})
 
 // timer function that counts down from 60 seconds when timer gets to 0, timer is cleared and game is ended
-const timer = () =>{  window.setInterval(function(){
-    count--;
-    console.log(count);
+window.timer = setInterval(function(){
+    if (start){
+        count--;
+    }
     document.getElementById('time-left').innerHTML = count.toString();
     if (count == 0){
         window.alert("game over! \n you ran out of time, \n try again");
         window.location.reload();
-        
-        window.clearInterval(timer());
+        window.clearInterval(window.timer);
     }
 }, 1000);
-}
+
+
 /*
 function that dynamically creates elements using template literals, via pushing objects into arrays
 and then defining the inner html of a empty div to be the position in that array via index.
@@ -83,7 +136,6 @@ function createQuestionCard(){
     quizQuestions.forEach((currentQuestion, i) => {
 
         const choices = [];
-
         for(x in currentQuestion.choices){
             choices.push(
                 `<label>
@@ -99,7 +151,12 @@ function createQuestionCard(){
             <button onclick="evalAnswers()" id="submit-button">submit</button>`
         );
     });
-    document.getElementById('question-card').innerHTML = output[index];
+    QC.innerHTML = output[index];
+    /*  
+        i am reseting the stopMultipleInputs with the creation of a new question card, 
+        this only allows the user to select one answer otherwise user can select multiple answers
+     */
+    stopMultipleInputs = 1;
 }
 
 //a simple sleep function that takes a milliseconds parameter and waits for that amount of time
@@ -116,28 +173,67 @@ finally checking if the user has completed all the questions, if the user has co
 the function displays the final score screen.
 */
 async function evalAnswers(){
-    if(document.querySelector('.check:checked').value == quizQuestions[index].answer){
-        score += count;
-        let correct = document.querySelector('.check:checked').closest("label");
-        correct.style.color = "Green";
+    if(stopMultipleInputs == 1){
+        if(document.querySelector('.check:checked')){
+            if(document.querySelector('#check:checked').value == quizQuestions[index].answer){
+                stopMultipleInputs = 2;
+                score += count;
+                let correct = document.querySelector('.check:checked').closest("label");
+                correct.style.color = "Green";
+                await sleep(3000);
+                count += 3;
+                index++;
+                moveon();
+            }else if(document.querySelector('.check:checked').value != quizQuestions[index].answer){
+                stopMultipleInputs = 2;
+                let wrong = document.querySelector('.check:checked').closest("label");
+                wrong.style.color = "red";
+                await sleep(3000);
+                count += 3;
+                index++;
+                moveon();
+            }
+        }else{
+            let op = document.createElement("h4");
+            op.innerText = "Please select a value";
+            QC.append(op);
+            await sleep(1000);
+            QC.removeChild(QC.lastChild);
+            return;
+        }
     }else{
-        let wrong = document.querySelector('.check:checked').closest("label");
-        wrong.style.color = "red";
+        return;
     }
-    await sleep(3000);
-    count += 3;
-    index++;
+}
+
+
+/*
+    this function was part of the evalAnswers function so i have seperated it into two functions. for the purpose making the code easier to read,
+    added highscore functionality that is pulling and storing data into localstorage.
+*/
+function moveon(){
     if(index != quizQuestions.length){
-        console.log(score);
         createQuestionCard();
     }else{
-        window.clearInterval(timer());
+        window.clearInterval(window.timer);
         document.getElementById('question-card').innerHTML = 
         `<h1>congratz</h1>
         <h2>Your Score was: ${score}!</h2>
-        <button id="return-button" >Return</button>
+        <label for ="score-name" >Enter your name for highscores</label>
+        <input id="score-name" type='text'> </input>
+        <button id="return-button" >submit</button>
         `;
-        document.getElementById("return-button").addEventListener('click', () => {window.location.reload();})
+        document.getElementById("return-button").addEventListener('click', () => { 
+            let tempArray;
+            if(JSON.parse(localStorage.getItem('highscores')) == null){
+                tempArray = highscore;
+            }else{
+                tempArray = JSON.parse(localStorage.getItem('highscores'));
+            }
+            tempArray.push({"name": document.getElementById("score-name").value, "score": score});
+            localStorage.setItem('highscores', JSON.stringify(tempArray));
+            window.location.reload();
+        })
     }
 }
     
